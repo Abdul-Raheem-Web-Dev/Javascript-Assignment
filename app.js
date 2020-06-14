620d90532cefdcab57f7f2f923414430782ea13b
Abdul-Raheem-Web-Dev <65037179+Abdul-Raheem-Web-Dev@users.noreply.github.com>
2020-06-14 19:50:26 +0500

    Add files via upload
    Assignment V

---
+++ app.js
@@ -0,0 +1,125 @@
+// Chapter # 1 - Start
+
+// Chapter # 1 - Task # 1
+
+alert("Hello");
+
+// Chapter # 1 - Task # 2
+
+alert("Error! Please enter a valid password.");
+
+// Chapter # 1 - Task # 3
+
+alert(" Welcome to JS Land... \n Happy Coding! ");
+
+// Chapter # 1 - Task # 4
+
+alert("Welcome to JS Land");
+alert("Happy Coding!");
+
+// Chapter # 1 - Task # 5
+
+alert("Hello... I can run JS through my web browser's console");
+
+// Chapter # 1 - Task # 6 (Not Possible at this level)
+
+// Chapter # 1 - Task # 7 (Not Possible at this level)
+
+// Chapter # 1 - End
+
+
+// Chapter # 2 - Start
+
+// Chapter # 2 - Task # 1
+
+var username;
+
+// Chapter # 2 - Task # 2
+
+var myname = "Abdul Raheem";
+
+// Chapter # 2 - Task # 3
+
+var message;
+message = "Hello World";
+alert(message);
+
+// Chapter # 2 - Task # 4
+
+var studentName = "Abdul Raheem";
+var studentAge = "22 years old";
+var studentCourse = "Certified Mobile Application Development";
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+// Chapter # 2 - Task # 5
+
+var pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P ";
+alert(pizza);
+
+// Chapter # 2 - Task # 6
+
+var email = "Rahimansari1697@gmail.com";
+alert("My email address is " + email);
+
+// Chapter # 2 - Task # 7
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// Chapter # 2 - Task # 8
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+// Chapter # 2 - Task # 9
+
+var symbStr = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+document.write(symbStr);
+alert(symbStr);
+
+// Chapter # 2 - End
+
+// Chapter # 3 - Start
+
+// Chapter # 3 - Task # 1
+
+var age = 22;
+alert("I am " + age + " years old");
+
+// Chapter # 3 - Task # 2
+
+var n = 14;
+alert("You have visited this site " + n + " times");
+
+// Chapter # 3 - Task # 3
+
+var birthYear = 1997;
+document.write("My birth year is " + birthYear + "<br /> Data type of my declared variable is number");
+
+// Chapter # 3 - Task # 4
+
+var visitorName = "John Doe";
+var productTittle = "T-Shirt";
+var productQuantity = 5;
+document.write(visitorName + " ordered " + productQuantity + " " + productTittle +"(s) on XYZ clothing store");
+
+// Chapter # 3 - End
+
+// Chapter # 4 - Start
+
+// Chapter # 4 - Task # 1
+
+var userName, userId, userPassword ;
+
+// Chapter # 4 - Task # 2
+
+var userName, $userAge, user_gender, _userEmail, userPhoneNumber_1;
+
+var 123, &userAge, user-gender, *userEmail, 1userPhoneNumber_1;
+
+// Chapter # 4 - Task # 3
+
+document.write("<h1>Rules for naming JS variables</h1> Vriable names can only contain letters, numbers, $ or _ . For example $my_1stVariable <br /> Variables must begin with a letter, $ or _ . For example $name, _name or name <br /> Variable names are case sensitive <br /> Variable names should not be JS keywords");
+
+// Chapter # 4 - End
